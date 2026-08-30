0f2a936766678cae8f6c3a184f14e592a55a6ac9
SebastianLai <surregograciano@gmail.com>
2026-08-30 13:17:25 -0500

    requirement-2-library

--- LibraryUdeA/src/main/java/com/juanfedevmaster/libraryudea/LibraryUdeA.java
+++ LibraryUdeA/src/main/java/com/juanfedevmaster/libraryudea/LibraryUdeA.java
@@ -6,24 +6,32 @@ package com.juanfedevmaster.libraryudea;
 import java.util.Scanner;
 import library.models.Book;
 import library.models.Catalog;
+import library.models.ResultBook;
 
 /**
+ * SEBASTIAN URREGO GRACIANO
+ * JUAN DAVID ORTIZ DIAZ
  *
- * @author juanfe
  */
 public class LibraryUdeA {
 
     public static void main(String[] args) {
 
         Catalog catalog = new Catalog();
+        catalog.registerBook("abc123", "Dance of dragons", "George R.R Martin", 150);
+        catalog.registerBook("bcd234", "Blood and fire", "George R.R Martin", 100);
 
         int option = 0;
         Scanner scan = new Scanner(System.in);
+        Book searchBook = null;
+
+
 
         do {
             System.out.println("1. Register book");
             System.out.println("2. Buy a book");
-            System.out.println("3. Exit");
+            System.out.println("4. Search for a book by ISBN");
+            System.out.println("5. Exit");
 
             System.out.println("Enter the option:");
             option = Integer.parseInt(scan.nextLine().trim());
@@ -38,21 +46,74 @@ public class LibraryUdeA {
                     String author = scan.nextLine().trim();
                     System.out.println("Enter the amount:");
                     int amount = Integer.parseInt(scan.nextLine().trim());
-                    catalog.registerBook(isbn, title, author, amount);
+
+                    ResultBook registerResult = catalog.registerBook(isbn, title, author, amount);
+
+                    if (registerResult == ResultBook.BOOK_EXIST) {
+                        System.out.println("---------------------------------------");
+                        System.out.println("There is already a book with that ISBN.");
+                        System.out.println("---------------------------------------");
+                    } else {
+                        System.out.println("-----------------------------");
+                        System.out.println("Book successfully registered.");
+                        System.out.println("-----------------------------");
+                    }
+
                     break;
                     
                 case 2: // Vender de Libro
                     System.out.println("Enter the isbn code of the book to buy:");
-                    String isbnSell = scan.next();
+                    String isbnSell = scan.nextLine();
 
                     System.out.println("Enter the amount of books:");
                     int amountSell = scan.nextInt();
+                    scan.nextLine();
 
-                    catalog.sellBook(isbnSell, amountSell);
+                    ResultBook salesResult = catalog.sellBook(isbnSell, amountSell);
+
+                    if (salesResult == ResultBook.SUCCESSFUL) {
+                        System.out.println("----------------------------------------");
+                        System.out.println("The purchase was completed successfully.");
+                        System.out.println("----------------------------------------");
+                    }
+                    else if(salesResult == ResultBook.NOT_STOCK) {
+                        System.out.println("---------------------------------------------");
+                        System.out.println("There are no books in stock for your purchase");
+                        System.out.println("---------------------------------------------");
+                    } else {
+                        System.out.println("--------------------------------------------------");
+                        System.out.println("There is no book with that ISBN. Please try again.");
+                        System.out.println("--------------------------------------------------");
+                    }
 
                     break;
                 case 3:
                     // Show all books of the catalog.
+                    break;
+                case 4:
+                    option = 5;
+                    System.out.println("Enter the ISBN of the book you want to find");
+                    String searchIsbn = scan.nextLine().trim();
+
+                    searchBook = catalog.findByIsbn(searchIsbn);
+
+                    if (searchBook == null) {
+                        System.out.println("--------------------------------------------------");
+                        System.out.println("There is no book with that ISBN. Please try again.");
+                        System.out.println("--------------------------------------------------");
+                        break;
+                    }
+
+                    System.out.println("--------------------------------------------------");
+                    System.out.println("Book found with the ISBN: " + searchBook.getIsbn());
+                    System.out.println("                                                   ");
+                    System.out.println("Book title: " + searchBook.getTitle());
+                    System.out.println("Author of the book: " + searchBook.getAuthor());
+                    System.out.println("Number of books available: " + searchBook.getAmount());
+                    System.out.println("--------------------------------------------------");
+
+                    searchBook = null;
+
                     break;
                 default:
                     option = 0;
